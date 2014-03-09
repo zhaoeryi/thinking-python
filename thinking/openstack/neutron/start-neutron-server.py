a2d4321c23dc6bad888bb7c35297876f1e3704b1
zhaoeryi <zhaoeryi@gmail.com>
2014-03-09 18:15:35 +0800

    Fixed start neutron server

--- thinking/openstack/neutron/start-neutron-server.py
+++ thinking/openstack/neutron/start-neutron-server.py
@@ -3,15 +3,11 @@
 import sys
 from neutron import server
 
-def start_neutron_server():
+if __name__ == "__main__":
     # ML2 Plugin
     # sys.argv = ['/usr/local/bin/neutron-server', '--config-file', '/etc/neutron/neutron.conf', '--config-file', '/etc/neutron/plugins/ml2/ml2_conf.ini']
     
     # OVS Plugin
     sys.argv = ['/usr/local/bin/neutron-server', '--config-file', '/etc/neutron/neutron.conf', '--config-file', '/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini']
     server.main()
-    
-        
-if __name__ == "__main__":
-    start_neutron_server()
 

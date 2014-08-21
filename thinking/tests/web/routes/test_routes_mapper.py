afe9b3345d0ad16ade4cd85b6b3ef102c55527bb
zhaoeryi <zhaoeryi@gmail.com>
2014-08-22 00:43:14 +0800

    Fixed test cases for python routes

--- thinking/tests/web/routes/test_routes_mapper.py
+++ thinking/tests/web/routes/test_routes_mapper.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 from thinking.tests import base
-import webob
 from thinking.web.dummy_controller import DummyController
 from routes import Mapper
 
@@ -15,102 +14,95 @@ class MapperConnectTestCase(base.ThinkingTestCase):
     def test_mapper_basic(self):
         route_name = self._testMethodName
         route_path = "/dummy"
-
-        map = Mapper()
-        map.connect(route_name, route_path,
-                        controller=DummyController(),
+        controller = DummyController()
+        
+        mapper = Mapper()
+        mapper.connect(route_name, route_path,
+                        controller=controller,
                         key1="value1", key2="value2")
 
         # test invalid url
         for invalid_url in ("/", "dummy", "/dummy/", "/dummy/123", "dummy123"):
-            match_dict = map.match(invalid_url)
+            match_dict = mapper.match(invalid_url)
             self.assertIsNone(match_dict)
 
         # test valid url
-        match_dict = map.match("/dummy")
-        self.assertIsInstance(match_dict, dict)
-        self.assertEquals(match_dict["key1"], "value1")
-        self.assertEquals(match_dict["key2"], "value2")
-        self.assertIsInstance(match_dict["controller"], unicode)
+        match_dict = mapper.match("/dummy")
+        self.assertDictEqual(match_dict, {'key2': u'value2', 'controller': str(controller).decode('utf-8'), 'key1': u'value1'})
 
     def test_mapper_with_placeholder(self):
         route_name = self._testMethodName
         route_path = "/dummy/{father_var}/{son_var}/{id_var}.html"
-
-        map = Mapper()
-        map.connect(route_name, route_path,
-                        controller=DummyController(),
+        controller = DummyController()
+        mapper = Mapper()
+        mapper.connect(route_name, route_path,
+                        controller=controller,
                         key1="value1", key2="value2")
 
-        #
-        match_dict = map.match("/dummy/father123/son456/789.html")
+        match_dict = mapper.match("/dummy/father123/son456/789.html")
         self.assertIsInstance(match_dict, dict)
-        self.assertEquals(match_dict["father_var"], "father123")
-        self.assertEquals(match_dict["son_var"], "son456")
-        self.assertEquals(match_dict["id_var"], "789")
+        
+        self.assertDictEqual(match_dict, {'father_var': u'father123', 'id_var': u'789', 'key2': u'value2', 'key1': u'value1', 'controller': str(controller).decode('utf-8'), 'son_var': u'son456'})
+        
 
     def test_mapper_with_regex(self):
         route_name = self._testMethodName
         route_path = "/dummy/{year}/{month}"
 
-        map = Mapper()
-        map.connect(route_name, route_path, controller=DummyController(),
+        mapper = Mapper()
+        mapper.connect(route_name, route_path, controller=DummyController(),
           action="view", year=2004,
           requirements=dict(year=R"\d{2013,2014}", month=R"\d{10,11}"))
 
         # test valid url
         for invalid_url in ("/dummy/2013/10", "/dummy/2013/11", "/dummy/2014/10", "/dummy/2014/11"):
-            match_dict = map.match(invalid_url)
+            match_dict = mapper.match(invalid_url)
             self.assertIsNone(match_dict)
 
         # test invalid url
         for invalid_url in ("/dummy/2013/9", "/dummy/2015/10"):
-            match_dict = map.match(invalid_url)
+            match_dict = mapper.match(invalid_url)
             self.assertIsNone(match_dict)
 
     def test_mapper_with_conditions(self):
         route_name = self._testMethodName
         route_path = "/dummy/fathers/"
-
-        map = Mapper()
-        map.connect(route_name, route_path,
-                        controller=DummyController(),
+        controller = DummyController()
+        
+        mapper = Mapper()
+        mapper.connect(route_name, route_path,
+                        controller=controller,
                         key1="value1", key2="value2", conditions=dict(method=["GET", "HEAD"]))
 
         #
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/dummy/fathers/", environ)
-        self.assertIsInstance(match_dict, dict)
+        match_dict = mapper.match("/dummy/fathers/", environ)
+        self.assertDictEqual(match_dict, {'key2': u'value2', 'key1': u'value1', 'controller': str(controller).decode('utf-8')})
         
         environ = {"REQUEST_METHOD":"HEAD"}
-        match_dict = map.match("/dummy/fathers/", environ)
-        self.assertIsInstance(match_dict, dict)
+        match_dict = mapper.match("/dummy/fathers/", environ)
+        self.assertDictEqual(match_dict, {'key2': u'value2', 'key1': u'value1', 'controller': str(controller).decode('utf-8')})
         
         environ = {"REQUEST_METHOD":"PUT"}
-        match_dict = map.match("/dummy/fathers/", environ)
+        match_dict = mapper.match("/dummy/fathers/", environ)
         self.assertIsNone(match_dict)
 
     def test_mapper_with_format(self):
         route_name = self._testMethodName
         route_path = "/dummy/fathers/{id_var}{.format}"
+        controller = DummyController()
 
-        map = Mapper()
-        map.connect(route_name, route_path,
-                        controller=DummyController(),
+        mapper = Mapper()
+        mapper.connect(route_name, route_path,
+                        controller=controller,
                         key1="value1", key2="value2")
 
         # Format is ".mp3"
-        match_dict = map.match("/dummy/fathers/123.mp3")
-        self.assertIsInstance(match_dict, dict)
-        self.assertEquals(match_dict["id_var"], "123")
-        self.assertEquals(match_dict["format"], "mp3")
-        
+        match_dict = mapper.match("/dummy/fathers/123.mp3")
+        self.assertDictEqual(match_dict, {'key2': u'value2', 'controller': str(controller).decode('utf-8'), 'format': u'mp3', 'key1': u'value1', 'id_var': u'123'})
         # Format is none
-        match_dict = map.match("/dummy/fathers/456")
-        self.assertIsInstance(match_dict, dict)
-        print(match_dict)
-        self.assertEquals(match_dict["id_var"], "456")
-        self.assertIsNone(match_dict["format"])
+        match_dict = mapper.match("/dummy/fathers/456")
+        self.assertDictEqual(match_dict, {'key2': u'value2', 'controller': str(controller).decode('utf-8'), 'format': None, 'key1': u'value1', 'id_var': u'456'})
         
         
         
--- thinking/tests/web/routes/test_routes_resources.py
+++ thinking/tests/web/routes/test_routes_resources.py
@@ -11,20 +11,20 @@ class ResourcesTestCase(base.ThinkingTestCase):
         member_name = "message" 
         collection_name = "messages"
 
-        map = Mapper()
-        map.resource(member_name, collection_name, collection={"rss": "GET"})
+        mapper = Mapper()
+        mapper.resource(member_name, collection_name, collection={"rss": "GET"})
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/messages/rss", environ)
+        match_dict = mapper.match("/messages/rss", environ)
         self.assertDictEqual(match_dict, {'action': u'rss', 'controller': u'messages'})
 
     def test_resources_with_member_action(self):
         member_name = "message" 
         collection_name = "messages"
 
-        map = Mapper()
-        map.resource(member_name, collection_name, member={'mark':'POST'})
+        mapper = Mapper()
+        mapper.resource(member_name, collection_name, member={'mark':'POST'})
         environ = {"REQUEST_METHOD":"POST"}
-        match_dict = map.match("/messages/123/mark", environ)
+        match_dict = mapper.match("/messages/123/mark", environ)
         self.assertDictEqual(match_dict, {'action': u'mark', 'controller': u'messages', 'id': u'123'})
        
 
@@ -32,10 +32,10 @@ class ResourcesTestCase(base.ThinkingTestCase):
         member_name = "message" 
         collection_name = "messages"
 
-        map = Mapper()
-        map.resource(member_name, collection_name, path_prefix="/category/{category_id}")
+        mapper = Mapper()
+        mapper.resource(member_name, collection_name, path_prefix="/category/{category_id}")
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/category/123/messages/456", environ)
+        match_dict = mapper.match("/category/123/messages/456", environ)
         self.assertDictEqual(match_dict, {'action': u'show', 'controller': u'messages', 'category_id': u'123', 'id': u'456'})
 
 
@@ -43,60 +43,60 @@ class ResourcesTestCase(base.ThinkingTestCase):
         member_name = "ip" 
         collection_name = "ips"
 
-        map = Mapper()
-        map.resource(member_name, collection_name,
+        mapper = Mapper()
+        mapper.resource(member_name, collection_name,
                             parent_resource=dict(member_name='server',
                                                  collection_name='servers'))
         
         # GET    /servers/123/ips
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/servers/123/ips", environ)
+        match_dict = mapper.match("/servers/123/ips", environ)
         self.assertDictEqual(match_dict, {'action': u'index', 'server_id': u'123', 'controller': u'ips'})
         
         # POST   /servers/123/ips
         environ = {"REQUEST_METHOD":"POST"}
-        match_dict = map.match("/servers/123/ips", environ)
+        match_dict = mapper.match("/servers/123/ips", environ)
         self.assertDictEqual(match_dict, {'action': u'create', 'server_id': u'123', 'controller': u'ips'})
         
         # GET    /servers/123/ips/new
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/servers/123/ips/new", environ)
+        match_dict = mapper.match("/servers/123/ips/new", environ)
         self.assertDictEqual(match_dict, {'action': u'new', 'server_id': u'123', 'controller': u'ips'})
         
         # PUT    /servers/123/ips/456
         environ = {"REQUEST_METHOD":"PUT"}
-        match_dict = map.match("/servers/123/ips/456", environ)
+        match_dict = mapper.match("/servers/123/ips/456", environ)
         self.assertDictEqual(match_dict, {'action': u'update', 'server_id': u'123', 'controller': u'ips', 'id': u'456'})
         
         
         # DELETE /servers/123/ips/456
         environ = {"REQUEST_METHOD":"DELETE"}
-        match_dict = map.match("/servers/123/ips/456", environ)
+        match_dict = mapper.match("/servers/123/ips/456", environ)
         self.assertDictEqual(match_dict, {'action': u'delete', 'server_id': u'123', 'controller': u'ips', 'id': u'456'})
         
         # GET    /servers/123/ips/456
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/servers/123/ips/456", environ)
+        match_dict = mapper.match("/servers/123/ips/456", environ)
         self.assertDictEqual(match_dict, {'action': u'show', 'server_id': u'123', 'controller': u'ips', 'id': u'456'})
         
         # GET    /servers/123/ips/456/edit
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/servers/123/ips/456/edit", environ)
+        match_dict = mapper.match("/servers/123/ips/456/edit", environ)
         self.assertDictEqual(match_dict, {'action': u'edit', 'server_id': u'123', 'controller': u'ips', 'id': u'456'})
         
         # GET    /servers/123/ips.{format}
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/servers/123/ips.mp3", environ)
+        match_dict = mapper.match("/servers/123/ips.mp3", environ)
         self.assertDictEqual(match_dict, {'action': u'index', 'server_id': u'123', 'controller': u'ips', 'format': u'mp3'})
         
         # GET    /servers/123/ips/new.mp3
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/servers/123/ips/new.mp3", environ)
+        match_dict = mapper.match("/servers/123/ips/new.mp3", environ)
         self.assertDictEqual(match_dict, {'action': u'new', 'server_id': u'123', 'controller': u'ips', 'format': u'mp3'})
         
         # GET    /servers/123/ips/456.mp3
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/servers/123/ips/456.mp3", environ)
+        match_dict = mapper.match("/servers/123/ips/456.mp3", environ)
         self.assertDictEqual(match_dict, {'action': u'show', 'server_id': u'123', 'controller': u'ips', 'id': u'456', 'format': u'mp3'})
         
         print(match_dict)
@@ -105,8 +105,8 @@ class ResourcesTestCase(base.ThinkingTestCase):
         member_name = "message" 
         collection_name = "messages"
 
-        map = Mapper()
-        map.resource(member_name, collection_name)
+        mapper = Mapper()
+        mapper.resource(member_name, collection_name)
         
         '''
         This establishes the following convention:
@@ -121,50 +121,50 @@ class ResourcesTestCase(base.ThinkingTestCase):
         
         # GET    /messages        => messages.index()    => url("messages")
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/messages", environ)
+        match_dict = mapper.match("/messages", environ)
         self.assertDictEqual(match_dict, {'action': u'index', 'controller': u'messages'})
         
         # POST   /messages        => messages.create()   => url("messages")
         environ = {"REQUEST_METHOD":"POST"}
-        match_dict = map.match("/messages", environ)
+        match_dict = mapper.match("/messages", environ)
         self.assertDictEqual(match_dict, {'action': u'create', 'controller': u'messages'})
 
         # GET    /messages/new    => messages.new()      => url("new_message")
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/messages/new", environ)
+        match_dict = mapper.match("/messages/new", environ)
         self.assertDictEqual(match_dict, {'action': u'new', 'controller': u'messages'})
         
         # PUT    /messages/1      => messages.update(id) => url("message", id=1)
         environ = {"REQUEST_METHOD":"PUT"}
-        match_dict = map.match("/messages/1", environ)
+        match_dict = mapper.match("/messages/1", environ)
         self.assertDictEqual(match_dict, {'action': u'update', 'controller': u'messages', 'id': u'1'})
         
         # DELETE /messages/1      => messages.delete(id) => url("message", id=1)
         environ = {"REQUEST_METHOD":"DELETE"}
-        match_dict = map.match("/messages/1", environ)
+        match_dict = mapper.match("/messages/1", environ)
         self.assertDictEqual(match_dict, {'action': u'delete', 'controller': u'messages', 'id': u'1'})
         
         # GET    /messages/1      => messages.show(id)   => url("message", id=1)
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/messages/1", environ)
+        match_dict = mapper.match("/messages/1", environ)
         self.assertDictEqual(match_dict, {'action': u'show', 'controller': u'messages', 'id': u'1'})
         
         # GET    /messages/1/edit => messages.edit(id)   => url("edit_message", id=1)
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/messages/1/edit", environ)
+        match_dict = mapper.match("/messages/1/edit", environ)
         self.assertDictEqual(match_dict, {'action': u'edit', 'controller': u'messages', 'id': u'1'})
         
         # GET    /messages.{format} => messages.edit(id)
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/messages.mp3", environ)
+        match_dict = mapper.match("/messages.mp3", environ)
         self.assertDictEqual(match_dict, {'action': u'index', 'controller': u'messages', 'format': u'mp3'})
         
         # GET    /messages/new.{format}    => messages.new()
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/messages/new.mp3", environ)
+        match_dict = mapper.match("/messages/new.mp3", environ)
         self.assertDictEqual(match_dict, {'action': u'new', 'controller': u'messages', 'format': u'mp3'})
         
         # GET    /messages/{id}.{format} => messages.show(id)
         environ = {"REQUEST_METHOD":"GET"}
-        match_dict = map.match("/messages/1.mp3", environ)
+        match_dict = mapper.match("/messages/1.mp3", environ)
         self.assertDictEqual(match_dict, {'action': u'show', 'controller': u'messages', 'id': u'1', 'format': u'mp3'})

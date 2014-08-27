8d1d626698c4d40779d1053fe42a40bb109260ac
zhaoeryi <zhaoeryi@gmail.com>
2014-08-27 18:51:37 +0800

    Add test cases for greenlet

---
+++ thinking/thread/greenlet_switch1.py
@@ -0,0 +1,14 @@
+import greenlet
+
+def test1(x, y):
+    z = gr2.switch(x + y)
+    print z
+
+def test2(u):
+    print u
+    gr1.switch(42)
+
+gr1 = greenlet.greenlet(test1)
+gr2 = greenlet.greenlet(test2)
+
+gr1.switch("hello", " world")

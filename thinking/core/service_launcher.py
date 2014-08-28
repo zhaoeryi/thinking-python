834b411824924ca5501043785fd6e11b43fadf73
zhaoeryi <zhaoeryi@gmail.com>
2014-08-29 01:14:00 +0800

    Update service launcher

--- thinking/core/service_launcher.py
+++ thinking/core/service_launcher.py
@@ -19,11 +19,190 @@ except ImportError:
     # Python 2.6
     UnsupportedOperation = None
 
-LOG = logging.getLogger(__name__)
 
+def _thread_done(gt, *args, **kwargs):
+    """Callback function to be passed to GreenThread.link() when we spawn()
+    Calls the :class:`ThreadGroup` to notify if.
+
+    """
+    print("Thread: GreenThread is done")
+    kwargs['group'].thread_done(kwargs['thread'])
+
+
+class Thread(object):
+    """Wrapper around a greenthread, that holds a reference to the
+    :class:`ThreadGroup`. The Thread will notify the :class:`ThreadGroup` when
+    it has done so it can be removed from the threads list.
+    """
+    def __init__(self, thread, group):
+        self.thread = thread
+        self.thread.link(_thread_done, group=group, thread=self)
+
+    def stop(self):
+        self.thread.kill()
+
+    def wait(self):
+        return self.thread.wait()
+
+    def link(self, func, *args, **kwargs):
+        self.thread.link(func, *args, **kwargs)
+
+
+class ThreadGroup(object):
+    """The point of the ThreadGroup class is to:
+
+    * keep track of timers and greenthreads (making it easier to stop them
+      when need be).
+    * provide an easy API to add timers.
+    """
+    def __init__(self, thread_pool_size=10):
+        self.pool = greenpool.GreenPool(thread_pool_size)
+        self.threads = []
+
+    def add_thread(self, callback, *args, **kwargs):
+        gt = self.pool.spawn(callback, *args, **kwargs)
+        th = Thread(gt, self)
+        self.threads.append(th)
+        return th
+
+    def thread_done(self, thread):
+        self.threads.remove(thread)
+
+    def stop(self):
+        current = threading.current_thread()
+
+        # Iterate over a copy of self.threads so thread_done doesn't
+        # modify the list while we're iterating
+        for x in self.threads[:]:
+            if x is current:
+                # don't kill the current thread.
+                continue
+            try:
+                x.stop()
+            except Exception as ex:
+                print(ex)
+
+    def wait(self):
+        current = threading.current_thread()
+
+        # Iterate over a copy of self.threads so thread_done doesn't
+        # modify the list while we're iterating
+        for x in self.threads[:]:
+            if x is current:
+                continue
+            try:
+                x.wait()
+            except eventlet.greenlet.GreenletExit:
+                pass
+            except Exception as ex:
+                print(ex)
+
+class Service(object):
+    """Service object for binaries running on hosts."""
+
+    def __init__(self, threads=1000):
+        self.tg = ThreadGroup(threads)
+
+        # signal that the service is done shutting itself down:
+        self._done = event.Event()
+
+    def reset(self):
+        # NOTE(Fengqian): docs for Event.reset() recommend against using it
+        self._done = event.Event()
+
+    def start(self):
+        pass
+
+    def stop(self):
+        self.tg.stop()
+        self.tg.wait()
+        # Signal that service cleanup is done:
+        if not self._done.ready():
+            self._done.send()
+
+    def wait(self):
+        self._done.wait()
+
+class Services(object):
+
+    def __init__(self):
+        self.services = []
+        self.tg = ThreadGroup()
+        self.done = event.Event()
+
+    def add(self, service):
+        self.services.append(service)
+        self.tg.add_thread(self.run_service, service, self.done)
+
+    def stop(self):
+        # wait for graceful shutdown of services:
+        for service in self.services:
+            service.stop()
+            service.wait()
+
+        # Each service has performed cleanup, now signal that the run_service
+        # wrapper threads can now die:
+        if not self.done.ready():
+            print("Services: send flag")
+            self.done.send()
+
+        # reap threads:
+        self.tg.stop()
+
+    def wait(self):
+        self.tg.wait()
+
+    def restart(self):
+        self.stop()
+        self.done = event.Event()
+        for restart_service in self.services:
+            restart_service.reset()
+            self.tg.add_thread(self.run_service, restart_service, self.done)
+
+    @staticmethod
+    def run_service(service, done):
+        """Service start wrapper.
+
+        :param service: service to run
+        :param done: event to wait on until a shutdown is triggered
+        :returns: None
+
+        """
+        service.start()
+        print("Services: before wait on flag")
+        done.wait()
+        print("Services: after wait on flag")
 
 def _sighup_supported():
     return hasattr(signal, 'SIGHUP')
+
+
+def _is_daemon():
+    # The process group for a foreground process will match the
+    # process group of the controlling terminal. If those values do
+    # not match, or ioctl() fails on the stdout file handle, we assume
+    # the process is running in the background as a daemon.
+    # http://www.gnu.org/software/bash/manual/bashref.html#Job-Control-Basics
+    try:
+        is_daemon = os.getpgrp() != os.tcgetpgrp(sys.stdout.fileno())
+    except OSError as err:
+        if err.errno == errno.ENOTTY:
+            # Assume we are a daemon because there is no terminal.
+            is_daemon = True
+        else:
+            raise
+    except UnsupportedOperation:
+        # Could not get the fileno for stdout, so we must be a daemon.
+        is_daemon = True
+    return is_daemon
+
+
+def _is_sighup_and_daemon(signo):
+    if not (_sighup_supported() and signo == signal.SIGHUP):
+        # Avoid checking if we are a daemon, because the signal isn't
+        # SIGHUP.
+        return False
+    return _is_daemon()
 
 
 def _signo_to_signame(signo):
@@ -48,12 +227,42 @@ class SignalExit(SystemExit):
 
 
 class ServiceLauncher(object):
+    """Launch one or more services and wait for them to complete."""
 
     def __init__(self):
-        self.pool = greenpool.GreenPool(10)
-        self.threads = []
-        self.done = event.Event()
+        """Initialize the service launcher.
 
+        :returns: None
+
+        """
+        self.services = Services()
+
+    def launch_service(self, service):
+        """Load and start the given service.
+
+        :param service: The service you would like to start.
+        :returns: None
+
+        """
+        self.services.add(service)
+
+    def stop(self):
+        """Stop all services which are currently running.
+
+        :returns: None
+
+        """
+        self.services.stop()
+
+
+    def restart(self):
+        """Reload config files and restart service.
+
+        :returns: None
+
+        """
+        self.services.restart()
+    
     def _handle_signal(self, signo, frame):
         # Allow the process to be killed again and die from natural causes
         _set_signals_handler(signal.SIG_DFL)
@@ -67,23 +276,10 @@ class ServiceLauncher(object):
         signo = 0
 
         try:
-
-            current = threading.current_thread()
-
-            # Iterate over a copy of self.threads so thread_done doesn't
-            # modify the list while we're iterating
-            for x in self.threads[:]:
-                if x is current:
-                    continue
-                try:
-                    # x.wait()
-                except eventlet.greenlet.GreenletExit:
-                    pass
-                except Exception as ex:
-                    print(ex)
+            self.services.wait()
         except SignalExit as exc:
             signame = _signo_to_signame(exc.signo)
-            print(('Caught SignalExit %s, exiting') % signame)
+            print(('Caught %s, exiting') % signame)
             status = exc.code
             signo = exc.signo
         except SystemExit as exc:
@@ -97,26 +293,16 @@ class ServiceLauncher(object):
         while True:
             self.handle_signal()
             status, signo = self._wait_for_exit_or_signal()
-
-    def launch_service(self, function, *args, **kwargs):
-        def func_wrapper():
-            function(*args, **kwargs)
-            print("start to wait")
-            self.done.wait()
-            print("finish wait")
-
-        gt = self.pool.spawn(func_wrapper, *args, **kwargs)
-        self.threads.append(gt)
-
-    def stop(self):
-        if not self.done.ready():
-            self.done.send()
-
-def hello_world():
-    print("hello world")
+            if not _is_sighup_and_daemon(signo):
+                return status
+            self.restart()
 
 if __name__ == '__main__':
-    print('\nStart to instantiate a new MyClass object')
+    class MyService(Service):
+        def start(self):
+            print("hello world")
+
+    service = MyService()
     launcher = ServiceLauncher()
-    launcher.launch_service(hello_world)
+    launcher.launch_service(service)
     launcher.wait()

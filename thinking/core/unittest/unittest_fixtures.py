b92fe76d4d2e9e8f50c0ee977e9bb22726d54af1
zhaoeryi <zhaoeryi@gmail.com>
2014-09-16 18:51:10 +0800

    Add cases for fixtures package

--- thinking/core/unittest/unittest_fixtures.py
+++ thinking/core/unittest/unittest_fixtures.py
@@ -1,31 +1,31 @@
 from __future__ import print_function
-import unittest
+import fixtures
+import testtools
 
-def setUpModule():
-    print("setUpModule")
- 
-def tearDownModule():
-    print("tearDownModule")
-    
-class MyTestCase(unittest.TestCase):
-
-    def my_fixture_setup(self):
-        print("++++ my_fixture_setup")
+class MyFixture(fixtures.Fixture):
+    def setUp(self):
+        super(MyFixture,self).setUp()
+        self.frobnozzle = 42
+        print("++++ MyFixture.setup()")
         
-    def my_fixture_cleanup(self):
-        print("++++ my_fixture_cleanup")
+    def cleanUp(self):
+        super(MyFixture,self).cleanUp()
+        print("++++ MyFixture.cleanup()")
         print()
+           
+class MyTestCase(testtools.TestCase):
            
     def setUp(self):
         super(MyTestCase, self).setUp()
-        self.my_fixture_setup()
-        self.addCleanup(self.my_fixture_cleanup)
+        self.my_fixture = self.useFixture(MyFixture())
+        print("++++ setUp")
 
     def tearDown(self):
         super(MyTestCase, self).tearDown()
         print("++++ tearDown")
         
     def test_case_1(self):
+        self.assertEqual(42, self.my_fixture.frobnozzle)
         print("++++ test case 1")
 
     def test_case_2(self):
